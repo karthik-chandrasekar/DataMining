5672e79b22b4ed5f3aae0841a1a0be6590ff6589
Karthik Chandrasekar <karthidec1@gmail.com>
2014-03-01 16:13:21 -0700

    Customizations in unit test script

--- BeautifulSpider.py
+++ BeautifulSpider.py
@@ -6,7 +6,7 @@ class BeautifulSpider:
     
     def __init__(self):
         self.movies_url = "http://www.snopes.com/movies/films/blucher.asp"
-        self.content_url = "http://www.snopes.com/movies/films/zhivago.asp"
+        self.content_url = "http://www.snopes.com/sports/baseball/ripkenstreak.asp"
    
     def run_main(self):
 
@@ -21,19 +21,22 @@ class BeautifulSpider:
                 if not link.split(":")[0] in junk_list:
                     #print link
                     pass               
-
         self.content()
-
     
     def content(self):
         page = urllib2.urlopen(self.content_url).read()
         soup = BeautifulSoup(page)
         main_content = soup.find_all("div", {"id":"main-content"})
+        main_content.extend(soup.find_all("div", {"class":"article_text"}))
+        main_content.extend(soup.find_all("div", {"class":"quoteBlockEnd"}))
+        main_content = [x for x in main_content if x]
+
+
         for samp in main_content:
             internal_id = samp.find_all("div")
+            print samp.getText()
             for samp2 in internal_id:
-                samp2.find_all("br")
-
+                print samp2.getText().encode('utf-8')
 
 if __name__ == "__main__":
     bs_obj = BeautifulSpider()
