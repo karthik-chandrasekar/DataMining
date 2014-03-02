1f3857be5ff236093fb18ea04dcb0d7e5adcea90
Karthik Chandrasekar <karthidec1@gmail.com>
2014-03-01 21:15:33 -0700

    Split using index values

--- ExtractOutput.py
+++ ExtractOutput.py
@@ -6,34 +6,52 @@ class ExtractOutput:
 
     def __init__(self):
         self.keywords = ['Claim:', 'Status:', 'Origins:', 'Sources:']
+        self.pattern = re.compile('[\W_]+')
 
     def run_main(self):
-        data = {}
-        pattern = re.compile('[\W_]+')
 
-        for fileName in os.listdir(os.curdir):
-  
+        #files = os.listdir(os.curdir)
+        files = ['movies-films-goldfinger.asp']
+        for fileName in files:
+            data = {}
+           
             try:
+
                 if (fileName.endswith(".py")):
                     continue
+
+                key_index  = []
                 for line in json.loads(open(fileName).read()):
-
                     for key in self.keywords:
-                        line = line.split(key)
-                        if len(line) >1:
-                            temp = line[0]
-                            temp = pattern.sub(" ", temp)
-                            data.setdefault(key, []).append(temp)
-                            line = " ".join(line[1:])
-                        else:
-                            line = " ".join(line) 
-
+                        key_index.append((key, line.find(key)))
+                self.splitData(key_index, data, line)  
                 open(fileName+"_new", "w").write(json.dumps(data))  
   
             except:
                 print "Exception for fileName - %s" % (fileName)
 
+    def splitData(self, key_index, data, line):
+        
+        cur_key = None
+        cur_index = None
 
+        for key, index in key_index:
+            if index == -1:continue
+            if not cur_key:
+                cur_key = key
+                cur_index = index
+                continue
+
+            data[cur_key] = self.dataClean(line[cur_index:index])
+            cur_key = key
+            cur_index = index
+        data[cur_key] = self.dataClean(line[cur_index:])
+
+
+    def dataClean(self, line):
+            
+        line = self.pattern.sub(" ", line).split(" ")
+        return " ".join(line[1:])
 
 if __name__ == "__main__":
     eo_obj = ExtractOutput()
