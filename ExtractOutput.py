c2c3ec077c1cc9180514526b64829d1cb496857b
Karthik Chandrasekar <karthidec1@gmail.com>
2014-03-01 23:42:23 -0700

    Remove last updated

--- ExtractOutput.py
+++ ExtractOutput.py
@@ -5,30 +5,42 @@ import re, json, os
 class ExtractOutput:
 
     def __init__(self):
-        self.keywords = ['Claim:', 'Status:', 'Origins:', 'Sources:']
+        self.keywords = ['Claim:', 'Status:', 'Origins:', 'Sources:', 'Last updated:']
         self.pattern = re.compile('[\W_]+')
 
     def run_main(self):
 
         #files = os.listdir(os.curdir)
-        files = ['movies-films-goldfinger.asp']
+        files = ['movies-actors-barrymore.asp']
         for fileName in files:
             data = {}
            
             try:
-
                 if (fileName.endswith(".py")):
                     continue
 
                 key_index  = []
                 for line in json.loads(open(fileName).read()):
+                    line = self.remLastUpdated(line)
                     for key in self.keywords:
                         key_index.append((key, line.find(key)))
-                self.splitData(key_index, data, line)  
-                open(fileName+"_new", "w").write(json.dumps(data))  
-  
+                self.splitData(key_index, data, line) 
+
+                final_string = "\n".join(data.values()) 
+                open(fileName+"_new", "w").write(final_string)  
             except:
                 print "Exception for fileName - %s" % (fileName)
+
+
+    def remLastUpdated(self, line):
+        lup_index = line.find('Last updated:')
+        sources_index = line.find('Sources:')
+        source_line = " "
+        if lup_index != -1:
+            if sources_index != -1:
+                source_line = line[sources_index:]
+            line = line[:lup_index] + source_line
+        return line
 
     def splitData(self, key_index, data, line):
         
@@ -42,16 +54,16 @@ class ExtractOutput:
                 cur_index = index
                 continue
 
-            data[cur_key] = self.dataClean(line[cur_index:index])
+            data[cur_key] = self.dataClean(line[cur_index:index], cur_key)
             cur_key = key
             cur_index = index
-        data[cur_key] = self.dataClean(line[cur_index:])
+        data[cur_key] = self.dataClean(line[cur_index:], cur_key)
 
-
-    def dataClean(self, line):
+    def dataClean(self, line, key):
             
+        key = self.pattern.sub("", key)
         line = self.pattern.sub(" ", line).split(" ")
-        return " ".join(line[1:])
+        return "@@@"+"begin"+key+"@@@"+" ".join(line[1:])+"@@@"+"end"+key+"@@@"
 
 if __name__ == "__main__":
     eo_obj = ExtractOutput()
