9ded02ca71cba521441e37014f0fdf6a25718bb0
Karthik Chandrasekar <karthidec1@gmail.com>
2014-03-05 21:39:26 -0700

    Status should be mixture if not true or false

--- ExtractOutput.py
+++ ExtractOutput.py
@@ -13,23 +13,26 @@ class ExtractOutput:
         #files = ['movies-actors-barrymore.asp']
         for fileName in files:
             data = {}
-            try:
-                if (fileName.endswith(".py")):
-                    continue
+            if (fileName.endswith(".py")):
+                continue
 
-                key_index  = []
-                for line in json.loads(open(fileName).read()):
-                    line = self.removeLastUpdated(line)
-                    for key in self.keywords:
-                        key_index.append((key, line.find(key)))
-                self.getData(key_index, data, line) 
-            
-                if not data:continue
+            key_index  = []
+            line = ""
+            try:
+                line  = json.loads(open(fileName).read())[0]
+                     
+                line = self.removeLastUpdated(line)
+                for key in self.keywords:
+                    key_index.append((key, line.find(key)))
+                self.getData(key_index, data, line)
+                if not 'Status:' in data and 'Claim:' in data: 
+                    self.statusCheck(data)
+                data = self.formatData(data) 
                 final_string = "\n".join(data.values()) 
                 open(fileName+"_new", "w").write(final_string)  
-            except:
-                print "Exception for fileName - %s" % (fileName)
 
+            except:
+                print "Exception for %s" % (fileName)
 
     def removeLastUpdated(self, line):
         lup_index = line.find('Last updated:')
@@ -52,16 +55,51 @@ class ExtractOutput:
                 cur_index = index
                 continue
 
-            data[cur_key] = self.dataClean(line[cur_index:index], cur_key)
+            data[cur_key] = self.dataClean(line[cur_index:index])
             cur_key = key
             cur_index = index
         
         if not cur_key or cur_index == -1:data= {};return
-        data[cur_key] = self.dataClean(line[cur_index:], cur_key)
+        data[cur_key] = self.dataClean(line[cur_index:])
 
-    def dataClean(self, line, key):
+    def dataClean(self, line):
         line = self.pattern.sub(" ", line).split(" ")
-        return "@@@"+"begin"+key+"@@@"+" ".join(line[1:])+"@@@"+"end"+key+"@@@"
+        return " ".join(line[1:])
+
+    def statusCheck(self, data):
+        claim = data.get('Claim:')    
+        claim = claim and claim.strip()
+        if not claim:return
+        claim = claim.split(" ")
+        status = claim[-1]
+        if status.lower() in ('true', 'false'):
+            data['Claim:'] = " ".join(claim[:-1])
+        else:
+            status = "Mixture"
+        data['Status:'] = status.strip()
+
+    def formatData(self, data):
+        new_data = {}
+        if not data:return
+        for key, value in data.iteritems():
+            if not value:continue
+            if key == "Status:":
+                value = self.cleanStatus(value)
+                print value
+            key = key.strip(":")
+            final_string = "@@@begin"+key+"@@@"+value+"@@@end"+key+"@@@"
+            new_data[key] = final_string
+        return new_data
+
+    def cleanStatus(self, value):
+        if not value:return
+        ex_index = value.find("Example") 
+        if not ex_index == -1: 
+            value =  value[:ex_index]
+        if value.lower().strip() not in ('true', 'false', 'mixture'):
+            value = 'Mixture'
+        return value    
+
 
 if __name__ == "__main__":
     eo_obj = ExtractOutput()
