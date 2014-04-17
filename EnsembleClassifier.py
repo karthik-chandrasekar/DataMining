a35fc76d5ac5219a1645b2c1b0b47f9ca1ba4526
Karthik Chandrasekar <karthidec1@gmail.com>
2014-04-17 15:50:46 -0700

    Running prototype

--- EnsembleClassifier.py
+++ EnsembleClassifier.py
@@ -4,6 +4,7 @@
 from scipy.sparse import lil_matrix
 from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
+import numpy
 
 class EnsembleClassifier:
 
@@ -20,7 +21,7 @@ class EnsembleClassifier:
         self.svm_classifier_output = "svm_classifier_output"
 
         #Sparse Matrix
-        self.sparse_matrix = lil_matrix((1843, 26365), dtype=float)
+        self.sparse_matrix = lil_matrix((1842, 26364), dtype=float)
         self.training_labels_list = []
 
     def load_input(self, filename):
@@ -32,6 +33,7 @@ class EnsembleClassifier:
         self.load_training_data()
         self.load_training_labels()
         self.load_test_data()
+        self.cross_validation()
 
     def load_training_data(self):
         for line in self.load_input(self.train_file):
@@ -39,20 +41,32 @@ class EnsembleClassifier:
             if not line:continue
             try:
                 row, col, val = line.split(' ')
-                row, col, val = int(row), int(col), float(val)
+                row, col, val = int(row)-1, int(col)-1, float(val)
                 self.sparse_matrix[row, col] = val
             except Exception,e:
                 print line
+          
+        #Convert lil_matrix to csc_matrix for efficient computing
+        #self.sparse_matrix = self.sparse_matrix.tocsc()    
+        self.sparse_matrix = self.sparse_matrix.toarray() 
+
     def load_training_labels(self):
         for line in self.load_input(self.label_training_file):
             line = line and line.strip()
             if not line:continue
-            line = float(line)
+            line = int(line)
             self.training_labels_list.append(line) 
+        self.training_labels_list = numpy.array(self.training_labels_list)
+        
 
     def load_test_data(self):
        pass 
 
+
+    def cross_validation(self):
+        clf = AdaBoostClassifier(n_estimators=100)
+        scores = cross_val_score(clf, self.sparse_matrix, self.training_labels_list)
+        print scores.mean()
 
 if __name__ == '__main__':
     ec = EnsembleClassifier()
